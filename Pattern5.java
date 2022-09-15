4e40d702dbfa7151140718d5863e1e8cf4d100cc
Shaswata Karan <shaswatakaran2016@gmail.com>
2022-09-16 02:59:51 +0530

    update

---
+++ Pattern5.java
@@ -0,0 +1,29 @@
+/*
+
+Inverted Half Pyramid (Rotated by 180)
+   *
+  **
+ ***
+****
+
+*/
+
+import java.util.*;
+
+public class Pattern5 {
+    public static void main(String args[]) {
+
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= 4; j++) {
+                if(j >= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+
+    }
+}
