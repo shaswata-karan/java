5bd193e667a297f2329ae6fb1d069353aa5ff77b
Shaswata Karan <shaswatakaran2016@gmail.com>
2022-10-12 23:31:27 +0530

    update

--- Function7.java
+++ Function7.java
@@ -3,19 +3,17 @@
 import java.util.*;
 
 public class Function7 {
-    public static int printTable(int num) {
+    public static void printTable(int num) {
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + " * " + i + " = " + num * i);
         }
-        
-        return num;
     }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
 
-        System.out.println(printTable(num));
+        printTable(num);
     }
 }
