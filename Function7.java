b38e8f80eb5383827cb3824c838128372df5d9f5
Shaswata Karan <shaswatakaran2016@gmail.com>
2022-10-12 23:26:04 +0530

    update

---
+++ Function7.java
@@ -0,0 +1,21 @@
+// Write a function to print the table of a given number n.
+
+import java.util.*;
+
+public class Function7 {
+    public static int printTable(int num) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+        
+        return num;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(printTable(num));
+    }
+}
