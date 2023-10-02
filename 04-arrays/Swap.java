6f1d9eae6b056a0d742505a3305a59c4a90d612d
Shaswata Karan <shaswatakaran2016@gmail.com>
2023-10-02 22:07:02 +0530

    add swap.java

---
+++ 04-arrays/Swap.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Swap {
+    public static void main(String[] args) {
+        int arr[] = {1,9,22,18,77};
+        swap(arr, 1, 3);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap (int arr[], int index1, int index3) {
+        int temp = 0;
+        temp = arr[index1];
+        arr[index1] = arr[index3];
+        arr[index3] = temp;
+    }
+}
