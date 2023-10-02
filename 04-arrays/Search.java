95e522fdfcc46fef3a06dbdc291cd9cd73564f57
Shaswata Karan <shaswatakaran2016@gmail.com>
2023-10-03 03:10:05 +0530

    Created array -> Search.java

---
+++ 04-arrays/Search.java
@@ -0,0 +1,30 @@
+// Take an array input from user. Search for a given number x and print the index where it occurs.
+
+import java.util.Scanner;
+public class Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int array[] = new int[size];
+
+        // input
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        // user inputs the element he wants to search
+        System.out.print("Enter the number you want to search: ");
+        int x = sc.nextInt();
+
+        // output
+        for (int i = 0; i < size; i++) {
+            if (array[i] == x) {
+                System.out.println("Element found at index: " + i);
+            }
+        }
+        sc.close();
+    }
+}
