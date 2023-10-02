93eed114d9dd659edab875764f1e1b6d3f758301
Shaswata Karan <shaswatakaran2016@gmail.com>
2023-10-03 00:19:30 +0530

    created array MaxValue.java

---
+++ 04-arrays/MaxValue.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaxValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The maximum value in the array is: " + max(arr,n));
+    }
+
+    public static int max (int arr[], int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
